523d59501735d7f3c9f8d9384de54c9de1d15953
Anindo Roy <anindoroy112@gmail.com>
2024-01-21 22:42:49 +0600

    incriment_update

---
+++ script.js
@@ -0,0 +1,16 @@
+let number=0;
+
+const button = document.querySelector("#button");
+const button1 = document.querySelector("#button1");
+const display = document.querySelector("#display");
+
+button.addEventListener("click",()=>{
+    number++;
+    display.textContent = number;
+
+});
+button1.addEventListener("click",()=>{
+    number--;
+    display.textContent = number;
+
+})
