728498babd67106a9d0aba19bd375acd2bbb07af
Anindo Roy <anindoroy112@gmail.com>
2024-02-07 22:21:18 +0600

    react_file_create

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,19 @@
-import Navbar from "./components/navbar";
+
 import "./App.css"
+import Contact from "./app/Contact/contact";
+import Intro from "./app/Intro/intro";
+import Navbar from "./app/Navbar/navbar";
+import Skills from "./app/Skills/skills";
+import Works from "./app/Works/work";
 
 function App() {
   return (
     <div className="App">
       <Navbar /> 
+      <Intro />
+      <Skills />
+      <Works />
+      <Contact/>
 
     </div>
   );
