275047063e7189ce1c543c73ce24c1825dbc3cd9
Anindo Roy <anindoroy112@gmail.com>
2024-07-14 11:31:46 +0600

    update

--- src/app/Intro/intro.js
+++ src/app/Intro/intro.js
@@ -1,29 +1,29 @@
 import React from "react";
-import './intro.css';
-import { Link } from "react-router-dom"
-import bg from '../../assets/background.png';
+import "./intro.css";
+import { Link } from "react-router-dom";
+import bg from "../../assets/background.png";
 import Hire from "./Hire/hire";
 
-
-
-
-const Intro = ()=>{
-    return(
-        <section id="intro">
-            <div className="introContent">
-                <span className="hello">Hello,,</span>
-                <span className="introText">I'm <span className="introName">Anindo</span><br />Web Devoloper</span>
-                <p className="introPara">I am a Skilled web devoloper with esperience in creating visually <br /> appealing and user friendly website.</p>
-                <Link to="/Hire">
-                <button className="btn" > 
-                    Hire Me
-                </button>
-                </Link>
-                
-
-            </div>
-            <img src={bg} alt="Profile" className="bg" />
-        </section>
-    )
-}
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,,</span>
+        <span className="introText">
+          I'm <span className="introName">Anindo</span>
+          <br />
+          Web Devoloper
+        </span>
+        <p className="introPara">
+          I am a Skilled web devoloper with esperience in creating visually{" "}
+          <br /> appealing and user friendly website.
+        </p>
+        <Link to="/Hire">
+          <button className="btn">Hire Me</button>
+        </Link>
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
+};
 export default Intro;
